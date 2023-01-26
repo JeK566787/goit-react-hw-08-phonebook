a8083accdcf268c21f2b88a4a4740dc0fcdfe758
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2023-01-26 18:26:24 +0200

    up

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,10 +1,14 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { logOut } from 'redux/auth/operations';
+import { selectUserEmail, selectUserName } from 'redux/auth/selectors';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const userEmail = useSelector(selectUserEmail);
+  const userName = useSelector(selectUserName);
 
   const handleClick = () => {
     dispatch(logOut());
@@ -13,7 +17,8 @@ const UserMenu = () => {
 
   return (
     <div>
-      <p>user email</p>
+      <p>{userName}</p>
+      <p>{userEmail}</p>
       <button type="button" onClick={handleClick}>
         LOG OUT
       </button>
--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -2,4 +2,7 @@ export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 
 export const selectUser = state => state.auth.user;
 
+export const selectUserEmail = state => state.auth.user.email;
+export const selectUserName = state => state.auth.user.name;
+
 export const selectIsRefreshing = state => state.auth.isRefreshing;
